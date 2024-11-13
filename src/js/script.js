de78d70064d0410bb549c1dc694ff88e33a130e1
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2024-11-13 15:10:09 +0100

    Ne marche pas : ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,9 @@ const nav = document.querySelector(".menu");
 
 // Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
 toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
   // Mise à jour des attributs ARIA pour accessibilité
   nav.ariaHidden = false;
   toggle.ariaExpanded = true;
